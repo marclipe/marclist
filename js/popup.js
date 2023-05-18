b1b4d4bd75ee0a329bd5e7e3d04fba039566591b
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-05-17 22:21:13 -0300

    New screen for Lists

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,46 @@
 window.addEventListener('DOMContentLoaded', function() {
-    const buttonCreate = document.querySelector(".button__create");
-    const popup = document.querySelector(".popup__list");
-    const closeBtn = this.document.querySelector('.button__cancel');
+  // popup
+  const buttonCreate = document.querySelector(".button__create");
+  const popup = document.querySelector(".popup__list");
+  const closeBtn = this.document.querySelector(".button__cancel");
 
-    buttonCreate.addEventListener('click', function() {
-        popup.style.display = 'block'
-    })
+  buttonCreate.addEventListener("click", function () {
+    popup.style.display = "block";
+  });
 
-    closeBtn.addEventListener('click', function() {
-        popup.style.display = 'none'
-    })
+  closeBtn.addEventListener("click", function () {
+    popup.style.display = "none";
+  });
+
+  const createList = document.querySelector(".button__continue");
+  createList.addEventListener('click', function(event) {
+      event.preventDefault();
+
+      const listNameInput = document.querySelector("#ilist");
+      const listName = listNameInput.value.trim();
+      if (listName === "") {
+        window.alert("Por favor coloque o nome para sua lista!");
+        return;
+      }
+
+      const emptyMessage = document.querySelector("#emptyMessage");
+      emptyMessage.remove();
+
+      const myContentList = document.querySelector(".itemList");
+
+      const inputValuList = listNameInput.value
+      myContentList.textContent = inputValuList
+      console.log(inputValuList)
+      myContentList.style.display = 'block';
+      myContentList.style.margin = 'auto';
+
+
+      // const shoppingListsContainer = document.querySelector(".shoppingListsContainer");
+
+      // // const newList = document.createElement("ul");
+      // // newList.innerHTML = "<li>Item 1</li>";
+      // // shoppingListsContainer.appendChild(newList);
+
+      popup.style.display = "none";
+  })
 })
